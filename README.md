bd851f079c6a4b981fe5ced27213b01dd619e419
VEX7FAR <fraamir3555@gmail.com>
2026-08-17 04:15:04 +0530

    added border and tested features

---
+++ README.md
@@ -0,0 +1,2 @@
+Basic2DP is a 2D translation physics engine that is capable of detecting collision, bouncing back, forces, etc.
+It is very simple as this will act as the base for a further bigger project however I might add more to it if I ever feel like it.
--- main.cpp
+++ main.cpp
@@ -1,21 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include<iostream>
+using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode({ 800,600 }), "Basic2DP");
+	window.setMaximumSize(sf::Vector2u{ 800,600 });
+	window.setVerticalSyncEnabled(true);	
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+	float thickness = 20.0;
+	sf::RectangleShape border(sf::Vector2f(800.0,600.0));
+	border.setFillColor(sf::Color::Black);
+	border.setOutlineThickness(-thickness);
+	border.setOutlineColor(sf::Color::Cyan);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	sf::CircleShape circle(100.0);
+	circle.setFillColor(sf::Color::Blue);
+	circle.setPosition(sf::Vector2f(200.0, 200.0));
+
+	sf::RectangleShape rect(sf::Vector2f(100, 150));
+	rect.setFillColor(sf::Color::Yellow);
+	rect.setPosition(sf::Vector2f(0, 0));
+
+	while (window.isOpen()) {
+		while (const optional event = window.pollEvent()) {
+			if (event->is < sf::Event::Closed>()) { window.close(); }
+		}
+
+		window.clear(sf::Color::Black);
+		window.draw(border);
+		window.draw(circle);
+		window.display();
+	}
 }
