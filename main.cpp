8bdd3ab91b2ca903207c8613a8b5bb62653905d6
VEX7FAR <fraamir3555@gmail.com>
2026-08-19 04:20:54 +0530

    defined coordinate

--- main.cpp
+++ main.cpp
@@ -48,27 +48,33 @@ int main()
 	unsigned int video_Y = 600 + (2 * thickness);
 	
 	sf::RenderWindow window(sf::VideoMode(sf::Vector2u{video_X,video_Y}), "ad");
-	window.setMaximumSize(sf::Vector2u{ 800,600 });
 	window.setVerticalSyncEnabled(true);
+	sf::View view;
+	view.setCenter(sf::Vector2f{400,300});
+	view.setSize(sf::Vector2f{ 840,-640 });
+	window.setView(view);
 
 	sf::RectangleShape border(sf::Vector2f(video_X, video_Y));
 	border.setFillColor(sf::Color::Black);
 	border.setOutlineThickness(-thickness);
 	border.setOutlineColor(sf::Color::Cyan);
+	border.setPosition(sf::Vector2f{ -20,-20 });
 
 	sf::CircleShape circle(100.0);
 	circle.setFillColor(sf::Color::Green);
 
-	float yscale;
-	sf::Shape* rect = shapemanager.createshape(rectange, sf::Vector2f{ 80, 60 }, sf::Vector2f{ 420,320 }, sf::Color::Magenta);
-
+	sf::Shape* rect = shapemanager.createshape(rectange, sf::Vector2f{ 10, 10 }, sf::Vector2f{ 0,0 }, sf::Color::Magenta);
+	rect->setOrigin(sf::Vector2f{ 0,0 });
 
 	while (window.isOpen()) {
+
 		window.clear(sf::Color::Black);
 		window.draw(border);
 		while (const optional event = window.pollEvent()) {
 			if (event->is < sf::Event::Closed>()) { window.close(); }
 		}
+
+		cout << window.getSize().x << " , " << window.getSize().y << endl;
 
 		window.draw(*rect);
 		window.display();
