346ca05c29af5de21d4f8c351ffc5a6cccb9a223
VEX7FAR <fraamir3555@gmail.com>
2026-08-22 04:09:49 +0530

    redid shapemaking and view

--- main.cpp
+++ main.cpp
@@ -9,15 +9,17 @@ enum shapetype {
 	circle, rectangle
 };
 
-sf::Shape* shapemaker(shapetype type, sf::Vector2f size, sf::Color color) {
+sf::Shape* shapemaker(shapetype type, sf::Vector2f size, sf::Color color = sf::Color::Blue) {
 	if (type == circle) {
 		sf::CircleShape circle(size.x);
 		circle.setFillColor(color);
+		circle.setOrigin(sf::Vector2f{ size.x , size.y });
 		return new sf::CircleShape(circle);
 	}
 	else if (type == rectangle) {
 		sf::RectangleShape rectange(size);
 		rectange.setFillColor(color);
+		rectange.setOrigin(sf::Vector2f{ size.x / (float)2.0, size.y / (float)2.0 });
 		return new sf::RectangleShape(rectange);
 	}
 	else
@@ -29,14 +31,33 @@ sf::Shape* shapemaker(shapetype type, sf::Vector2f size, sf::Color color) {
 int main() {
 	sf::View view;
 	sf::RenderWindow window(sf::VideoMode({840,640}), "adad");
-	window.setVerticalSyncEnabled(true);
 	window.setFramerateLimit(60);
+	view.setSize({840,-640});
+	view.setCenter({0,0});
+	window.setView(view);
 	
-	window.display();
+	sf::Shape* circle = shapemaker(shapetype::circle, { 20,20 });
+	circle->setPosition({ 0,0 });
+
+	sf::Shape* circle2 = shapemaker(shapetype::circle, { 20,20 }, sf::Color::Cyan);
+	circle2->setPosition({ 110,110 });
+
+	sf::Shape* border = shapemaker(shapetype::rectangle, { 840,640 }, sf::Color::Transparent);
+	border->setOrigin(sf::Vector2f{ 420.0,320.0 });
+	border->setOutlineThickness(-20);
+	border->setOutlineColor(sf::Color::Red);
+
 	while (window.isOpen()) {
-		if (const std::optional event = window.pollEvent()) {
+		window.clear();
+		while (const std::optional event = window.pollEvent()) {
 			if (event->is<sf::Event::Closed>()) { window.close(); }
 		}
-		cout << "FAKJHD";
+
+		window.draw(*border);
+		window.draw(*circle);
+		window.draw(*circle2);
+		window.display();
+
+		cout << window.getSize().y << endl;
 	}
 }
